d2fe80475ad96f3578ba3297b306eb772ddec0f5
neil patel <neil@myindianweddingapp.com>
2015-09-23 19:03:05 +0100

    Added tests for delete method

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,16 +1,15 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
+
 
   def index
-    @todo_list = TodoList.find(params[:todo_list_id])
   end
 
   def new
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new
   end
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_items_params)
     
     if @todo_item.save
@@ -23,7 +22,6 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
@@ -41,11 +39,25 @@ class TodoItemsController < ApplicationController
     end
 
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted."
+    else
+      flash[:error] = "Todo list item was deleted."
+    end
+    redirect_to todo_list_todo_items_path
+  end
+
   def url_options
       { todo_list_id: params[:todo_list_id] }.merge(super)
   end
 
   private 
+
+  def find_todo_list
+     @todo_list = TodoList.find(params[:todo_list_id])
+  end
 
   def todo_items_params
     params[:todo_item].permit(:content)
