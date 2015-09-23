0c75edc0d65dc930db010a363b2e071e18bb33be
neil patel <neil@myindianweddingapp.com>
2015-09-23 16:57:59 +0100

    Add specs and code for editing items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,4 +1,53 @@
 class TodoItemsController < ApplicationController
+
   def index
+    @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def new
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new
+  end
+
+  def create
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.new(todo_items_params)
+    
+    if @todo_item.save
+      flash[:success] = "Added todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "There was a problem adding that todo list item"
+      render action: :new
+    end
+  end
+
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  end
+
+  def update
+      @todo_list = TodoList.find(params[:todo_list_id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    
+      if @todo_item.update_attributes(todo_list_params)
+        flash[:success] = "Saved todo list item."
+        redirect_to todo_list_items_path
+      else
+        flash[:error] = "That todo item could not be saved."
+        render action: :edit
+      end
+    end
+
+
+  def url_options
+      { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
+
+  private 
+
+  def todo_items_params
+    params[:todo_item].permit(:content)
   end
 end
---
+++ spec/features/todo_items/create_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper'
+
+describe "Adding todo items" do
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery list") }
+
+
+  def visit_todo_list(list)
+    visit "/todo_lists"
+    within "#todo_list_#{list.id}" do
+      click_link "List Items"
+    end
+  end
+
+  it "is successful with valid content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+
+    fill_in "Content", with: "Milk"
+    click_button "Save"
+
+    expect(page).to have_content("Added todo list item.")
+    within("il.todo_items") do
+      expect(page).to have_content("Milk")
+  end 
+end
+
+  it "displays an error with no content" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: ""
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that tdo list item.")
+  end
+      expect(page).to have_content("Content can't be blank")
+end
+
+it "displays an error with no content less then 2 characters long" do
+    visit_todo_list(todo_list)
+    click_link "New Todo Item"
+    fill_in "Content", with: "1"
+    click_button "Save"
+    within("div.flash") do
+      expect(page).to have_content("There was a problem adding that tdo list item.")
+  end
+      expect(page).to have_content("Content is too short")
+end
+end
