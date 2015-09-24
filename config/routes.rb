4b19c5a47eebe8cd83611c941e6acc97556423fc
neil patel <neil@myindianweddingapp.com>
2015-09-24 10:41:33 +0100

    Make todo item as complete

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
 
 
   resources :todo_lists do
-    resources :todo_items
+    resources :todo_items do
+    member do
+      patch :complete
+    end
   end
+end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
