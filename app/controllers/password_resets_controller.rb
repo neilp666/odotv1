3aec590f8dd8697efadc49b789b1d358542f240a
neil patel <neil@myindianweddingapp.com>
2015-09-27 19:55:48 +0100

    Added text for password resets

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,6 +6,7 @@ class PasswordResetsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
     user.generate_password_reset_token!
+    Notifier.password_reset(user).deliver
     redirect_to login_path
   end
 end
