90597f63231d5f57623085ba4fea3f48821f6866
neil patel <neil@myindianweddingapp.com>
2015-09-28 11:44:32 +0100

    Added password reset controller & routes

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -5,8 +5,22 @@ class PasswordResetsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    user.generate_password_reset_token!
-    Notifier.password_reset(user).deliver
-    redirect_to login_path
+    if user
+      user.generate_password_reset_token!
+      Notifier.password_reset(user).deliver
+      flash[:success] = "Password reset instructions sent! Please check your email."
+      redirect_to login_path
+    else
+      flash.now[:notice] = "Email not found."
+      render action: 'new'
   end
+end
+
+    def edit
+      @user = User.find_by(password_reset_token: params[:id])
+      if @user
+      else
+        render file: 'public/404.html', status: :not_found
+      end
+    end
 end
--- spec/controllers/password_resets_controller_spec.rb
+++ spec/controllers/password_resets_controller_spec.rb
@@ -22,9 +22,51 @@ describe PasswordResetsController do
         expect{ post :create, email: user.email; user.reload }.to change{user.password_reset_token}
     end
 
+
       it "sends a password reset email" do
         expect{ post :create, email: user.email }.to change(ActionMailer::Base.deliveries, :size)
     end
+
+    it "sets the flash success message" do
+      post :create, email: user.email
+      expect(flash[:succss]).to match(/check your email/)
+  end
+  
+      context "with no user found" do
+        it "renders the new page" do
+          post :create, email: 'none@found.com'
+          expect(response).to render_template('new')
+    end
+  end
+
+    it "sets the flash message" do
+      post :create, email: 'none@found.com'
+      expect(flash[:notice]).to match(/not found/)
   end
 end
+end
+
+    describe "GET edit" do
+      context "with a valid password_reset_token" do
+        let(:user) { create(:user) }
+        before { user.generate_password_reset_token! }
+
+        it "renders the edit template" do
+          get :edit, id: user.password_reset_token
+          expect(response).to render_template('edit')
+       end
+
+       it "assigns a @user" do
+        get :edit, id: user.password_reset_token
+        expect(assigns(:user)).to eq(user)
+  end
+end
+        context "with no password_reset_token found" do
+          it "renders the 404 page" do
+            get :edit, id: 'notfound'
+            expect(response.status).to eq(404)
+            expect(response).to render_template(file: "#{Rails.root}/public/404.html")
+        end
+      end
+    end
 end
